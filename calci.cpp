cbfc2fcf36af956c0d7fd1e005f5018bc19a7ed4
Suhani5678 <56391109+Suhani5678@users.noreply.github.com>
2021-07-12 13:24:25 +0530

    Create calci.cpp

---
+++ calci.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float a, b;
+
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Enter two operands:";
+    cin >> a >> b;
+
+    switch(op) {
+      case '+':
+          cout << a << "+" << b << "=" << a + b;
+      case '-':
+          cout << a << "-" << b << "=" << a - b;
+      case '*':
+          cout << a << "*" << b << "=" << a * b;
+      case '/':
+          cout << a << "/" << b << "=" << a / b;
+          breake;
+        
+      default:
+          cout << "Operator is not correct";
+        breake;
+    }
+    return 0;
+}
